007582a7c882a5717d888c0dac41334225f154c1
Rackar <27681135@qq.com>
2022-06-14 01:42:02 +0800

    能命中目标

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -5,38 +5,125 @@
         <ion-title>Typing Soup</ion-title>
       </ion-toolbar>
     </ion-header>
-    
+
     <ion-content :fullscreen="true">
       <ion-header collapse="condense">
         <ion-toolbar>
           <ion-title size="large">Blank</ion-title>
         </ion-toolbar>
       </ion-header>
-    
-      <div id="container">
-         {{wordsList}}
 
-         <button @click="startGetWord">start Get Word</button>
+      <div id="container">
+        {{ wordStart }}
+        {{ wordMiddle }}
+        {{ wordEnd }}
+        <button @click="startGetWord">start Get Word</button>
+        <input :value="info.input" class="input" @input="onInputChange"
+          placeholder="Tap on the virtual keyboard to start">
+        <SimpleKeyboard @onChange="onChange" @onKeyPress="onKeyPress" @keyboardMounted="keyboardMounted"
+          :input="info.input" />
       </div>
     </ion-content>
   </ion-page>
 </template>
 
 <script lang="ts">
+import SimpleKeyboard from "./KeyBoard.vue";
 import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/vue';
-import { defineComponent,reactive } from 'vue';
+import { defineComponent, reactive, computed } from 'vue';
 // var randomWords = require('random-words');
-import {randomWords} from '../libs/randomWord';
+import { randomWords } from '../libs/randomWord';
+interface Iwordlist {
+  word: string;
+  key: string;
+}
 
-let wordsList:string[] =reactive([]) 
+let wordsList: Iwordlist[] = reactive([])
+let info = reactive({
+  input: "",
+  aimKeys: ['q', 'a', 'z', 'e', 'i', 'o']
+})
+let wordStart = computed(() => {
+  let el = wordsList[0]
+  if (el) {
+    let word = el.word
+    let key = el.key
+
+    return word.split(key)[0]
+  } else {
+    return ''
+  }
+})
+
+let wordMiddle = computed(() => {
+  let el = wordsList[0]
+  if (el) {
+    return el.key
+  } else {
+    return ''
+  }
+})
+
+let wordEnd = computed(() => {
+  let el = wordsList[0]
+  if (el) {
+    let word = el.word
+    let key = el.key
+
+    return word.split(key)[1]
+  } else {
+    return ''
+  }
+})
+
 function startGetWord() {
   setInterval(() => {
-   let arr= randomWords(15).filter((word:string)=>word.indexOf('a')!==-1)
-    wordsList.push(...arr)
+    let arr = randomWords(5)
+
+    for (const word of arr) {
+      let res = checkWordsHasAimkey(word, info.aimKeys)
+      if (res) {
+        wordsList.push(res)
+      }
+    }
   }, 1000);
+}
+function checkWordsHasAimkey(word: string, aimkey: string[]): Iwordlist | null {
+  {
+    for (const keychar of aimkey) {
+      if (word.indexOf(keychar) !== -1) {
+        return { word, key: keychar }
+      }
+    }
+    return null
+  }
+}
+
+let keyboard: any = null
+function keyboardMounted(_keyboard: any) {
+  keyboard = _keyboard
 
 }
 
+function onChange(_input: string) {
+  console.log("Input changed", _input);
+  info.input = _input;
+}
+function onKeyPress(button: string) {
+  console.log("button", button);
+  let cur = wordsList[0]
+  if (cur && cur.key === button) {
+    keyboard.removeButtonTheme(button, "next-button");
+    wordsList.shift()
+    let next = wordsList[0]
+    if (next)
+      keyboard.addButtonTheme(next.key, "next-button");
+  }
+}
+function onInputChange(_input: any) {
+  console.log("Input changed", _input);
+  info.input = _input.target.value;
+}
 export default defineComponent({
   name: 'HomePage',
   components: {
@@ -44,20 +131,25 @@ export default defineComponent({
     IonHeader,
     IonPage,
     IonTitle,
-    IonToolbar
-  },setup() {
-      return {
-        wordsList,
-        startGetWord
-      }
+    IonToolbar,
+    SimpleKeyboard,
+
+  }, setup() {
+    return {
+      wordsList,
+      startGetWord,
+      info, onChange, onKeyPress, onInputChange,
+      wordStart, wordMiddle, wordEnd,
+      keyboardMounted
     }
+  }
 });
 </script>
 
 <style scoped>
 #container {
   text-align: center;
-  
+
   position: absolute;
   left: 0;
   right: 0;
@@ -73,13 +165,31 @@ export default defineComponent({
 #container p {
   font-size: 16px;
   line-height: 22px;
-  
+
   color: #8c8c8c;
-  
+
   margin: 0;
 }
 
 #container a {
   text-decoration: none;
+}
+</style>
+<style>
+input {
+  width: 100%;
+  height: 100px;
+  padding: 20px;
+  font-size: 20px;
+  border: none;
+  box-sizing: border-box;
+}
+
+.simple-keyboard {
+  max-width: 850px;
+}
+
+.next-button {
+  border: 2px solid #ff0000;
 }
 </style>
---
+++ src/views/KeyBoard.vue
@@ -0,0 +1,69 @@
+<template>
+  <div :class="keyboardClass"></div>
+</template>
+
+<script>
+import Keyboard from "simple-keyboard";
+import "simple-keyboard/build/css/index.css";
+
+  export default {
+    name: "SimpleKeyboard",
+  props: {
+    keyboardClass: {
+      default: "simple-keyboard",
+      type: String
+    },
+    input: {
+      type: String
+    }
+  },
+  data: () => ({
+    keyboard: null
+  }),
+  mounted() {
+    this.keyboard = new Keyboard(this.keyboardClass, {
+      physicalKeyboardHighlight: true,//实体键盘敲击高亮
+      // physicalKeyboardHighlightPress: true,
+      layoutName: "default", //修改布局
+      onChange: this.onChange,
+      onKeyPress: this.onKeyPress
+    });
+    this.$emit('keyboardMounted', this.keyboard);
+    document.onkeydown = (e) => {
+      console.log('原生', e.key)
+      this.$emit("onKeyPress", e.key);
+
+    }
+  },
+  methods: {
+    onChange(input) {
+      this.$emit("onChange", input);
+    },
+    onKeyPress(button) {
+      this.$emit("onKeyPress", button);
+
+      /**
+       * If you want to handle the shift and caps lock buttons
+       */
+      if (button === "{shift}" || button === "{lock}") this.handleShift();
+    },
+    handleShift() {
+      let currentLayout = this.keyboard.options.layoutName;
+      let shiftToggle = currentLayout === "default" ? "shift" : "default";
+
+      this.keyboard.setOptions({
+        layoutName: shiftToggle
+      });
+    }
+  },
+  watch: {
+    input(input) {
+      this.keyboard.setInput(input);
+    }
+  }
+};
+</script>
+
+
+<style>
+</style>
