3c2bd2d12074237fbc42213416ca029f4d7ae109
Rackar <27681135@qq.com>
2022-06-13 23:29:19 +0800

    init

---
+++ src/views/HomePage.vue
@@ -0,0 +1,85 @@
+<template>
+  <ion-page>
+    <ion-header :translucent="true">
+      <ion-toolbar>
+        <ion-title>Typing Soup</ion-title>
+      </ion-toolbar>
+    </ion-header>
+    
+    <ion-content :fullscreen="true">
+      <ion-header collapse="condense">
+        <ion-toolbar>
+          <ion-title size="large">Blank</ion-title>
+        </ion-toolbar>
+      </ion-header>
+    
+      <div id="container">
+         {{wordsList}}
+
+         <button @click="startGetWord">start Get Word</button>
+      </div>
+    </ion-content>
+  </ion-page>
+</template>
+
+<script lang="ts">
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/vue';
+import { defineComponent,reactive } from 'vue';
+// var randomWords = require('random-words');
+import {randomWords} from '../libs/randomWord';
+
+let wordsList:string[] =reactive([]) 
+function startGetWord() {
+  setInterval(() => {
+   let arr= randomWords(15).filter((word:string)=>word.indexOf('a')!==-1)
+    wordsList.push(...arr)
+  }, 1000);
+
+}
+
+export default defineComponent({
+  name: 'HomePage',
+  components: {
+    IonContent,
+    IonHeader,
+    IonPage,
+    IonTitle,
+    IonToolbar
+  },setup() {
+      return {
+        wordsList,
+        startGetWord
+      }
+    }
+});
+</script>
+
+<style scoped>
+#container {
+  text-align: center;
+  
+  position: absolute;
+  left: 0;
+  right: 0;
+  top: 50%;
+  transform: translateY(-50%);
+}
+
+#container strong {
+  font-size: 20px;
+  line-height: 26px;
+}
+
+#container p {
+  font-size: 16px;
+  line-height: 22px;
+  
+  color: #8c8c8c;
+  
+  margin: 0;
+}
+
+#container a {
+  text-decoration: none;
+}
+</style>
